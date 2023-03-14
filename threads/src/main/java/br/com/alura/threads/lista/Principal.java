7cc8f46ae900d0de0158e385c6487e9241d6fe07
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-03-14 02:56:58 -0300

    Usar Collections.synchronizedList

--- threads/src/main/java/br/com/alura/threads/lista/Principal.java
+++ threads/src/main/java/br/com/alura/threads/lista/Principal.java
@@ -1,13 +1,15 @@
 package br.com.alura.threads.lista;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Principal {
 
     public static void main(String[] args) throws InterruptedException {
 
-        List<String> lista = new ArrayList<String>(); //ArrayList is not thread-safe
+        //List<String> lista = new ArrayList<String>(); //ArrayList is not thread-safe
+        List<String> lista = Collections.synchronizedList(new ArrayList<String>()); //this is thread-safe
         //List<String> lista = new Vector<String>(); //Vector is thread-safe
 
         for (int i = 0; i < 10; i++) {
