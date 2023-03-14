a532a371644416e46b6749447d331e0bf4030a6a
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-03-14 02:51:11 -0300

    Usar synchronized para resolver problema na lista customizada

--- threads/src/main/java/br/com/alura/threads/lista/customizada/Lista.java
+++ threads/src/main/java/br/com/alura/threads/lista/customizada/Lista.java
@@ -4,7 +4,7 @@ public class Lista {
     private String[] elementos = new String[1000];
     private int indice = 0;
 
-    public void adicionaElementos(String elemento) {
+    public synchronized void adicionaElementos(String elemento) {
         this.elementos[indice] = elemento;
         this.indice++;
     }
