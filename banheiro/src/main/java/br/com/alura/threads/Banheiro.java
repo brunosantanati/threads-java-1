bba2004fe1880bd7708fd0486f76078848fe5ad2
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-03-14 03:20:54 -0300

    Implementar esperaLaFora quando banheiro esta sujo

--- banheiro/src/main/java/br/com/alura/threads/Banheiro.java
+++ banheiro/src/main/java/br/com/alura/threads/Banheiro.java
@@ -2,12 +2,20 @@ package br.com.alura.threads;
 
 public class Banheiro {
 
+    private boolean estaSujo = true;
+
     public void fazNumero1() {
 
         String nome = Thread.currentThread().getName();
+        System.out.println(nome + " batendo na porta");
 
         synchronized (this) {
             System.out.println(nome + " entrando no banheiro");
+
+            if (this.estaSujo) {
+                esperaLaFora(nome);
+            }
+
             System.out.println(nome + " fazendo coisa rapida");
 
             try {
@@ -25,9 +33,15 @@ public class Banheiro {
     public void fazNumero2() {
 
         String nome = Thread.currentThread().getName();
+        System.out.println(nome + " batendo na porta");
 
         synchronized (this) {
             System.out.println(nome + " entrando no banheiro");
+
+            if (this.estaSujo) {
+                esperaLaFora(nome);
+            }
+
             System.out.println(nome + " fazendo coisa demorada");
 
             try {
@@ -39,6 +53,16 @@ public class Banheiro {
             System.out.println(nome + " dando descarga");
             System.out.println(nome + " lavando a mao");
             System.out.println(nome + " saindo do banheiro");
+        }
+    }
+
+    private void esperaLaFora(String nome) {
+
+        System.out.println(nome + ", eca, banheiro está sujo");
+        try {
+            this.wait();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
