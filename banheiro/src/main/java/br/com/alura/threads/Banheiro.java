7211e8f3587bf332f4ab13e673128bd5c731d06b
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2023-03-14 02:05:23 -0300

    Usar synchronized
    Usando synchronized para evitar que 2 convidados usem o banheiro ao mesmo tempo

--- banheiro/src/main/java/br/com/alura/threads/Banheiro.java
+++ banheiro/src/main/java/br/com/alura/threads/Banheiro.java
@@ -6,35 +6,39 @@ public class Banheiro {
 
         String nome = Thread.currentThread().getName();
 
-        System.out.println(nome + " entrando no banheiro");
-        System.out.println(nome + " fazendo coisa rapida");
+        synchronized (this) {
+            System.out.println(nome + " entrando no banheiro");
+            System.out.println(nome + " fazendo coisa rapida");
 
-        try {
-            Thread.sleep(8000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            try {
+                Thread.sleep(8000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(nome + " dando descarga");
+            System.out.println(nome + " lavando a mao");
+            System.out.println(nome + " saindo do banheiro");
         }
-
-        System.out.println(nome + " dando descarga");
-        System.out.println(nome + " lavando a mao");
-        System.out.println(nome + " saindo do banheiro");
     }
 
     public void fazNumero2() {
 
         String nome = Thread.currentThread().getName();
 
-        System.out.println(nome + " entrando no banheiro");
-        System.out.println(nome + " fazendo coisa demorada");
+        synchronized (this) {
+            System.out.println(nome + " entrando no banheiro");
+            System.out.println(nome + " fazendo coisa demorada");
 
-        try {
-            Thread.sleep(15000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            try {
+                Thread.sleep(15000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(nome + " dando descarga");
+            System.out.println(nome + " lavando a mao");
+            System.out.println(nome + " saindo do banheiro");
         }
-
-        System.out.println(nome + " dando descarga");
-        System.out.println(nome + " lavando a mao");
-        System.out.println(nome + " saindo do banheiro");
     }
 }
